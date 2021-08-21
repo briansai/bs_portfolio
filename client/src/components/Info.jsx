17a68ce8eaae12d0fa841acec585ef3796f72709
Brian Sai <brianwsai@gmail.com>
2021-08-20 17:11:25 -0700

    Implemented typewriter animation

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './Info.scss';
 
 function Info() {
-  return <h1 className="info">Brian Sai</h1>;
+  return (
+    <div className="info">
+      <h1 className="info__name">Brian Sai</h1>
+      <h2 className="info__welcome">WELCOME TO MY PORTFOLIO!</h2>
+      <div className="info__typewriter">
+        <div className="info__typewriter--content">Full Stack Developer</div>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
