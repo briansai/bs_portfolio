4e54f4524eb0dfd8f638114c0062657f6027c372
Brian Sai <brianwsai@gmail.com>
2021-08-25 16:13:47 -0700

    Initialized projects component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header';
 import Intro from './components/Intro';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Intro />
+      <Projects />
     </div>
   );
 }
