270f0f7e0b84117e9908ecafe7ca0f6c0223189d
Brian Sai <brianwsai@gmail.com>
2021-09-01 17:18:22 -0700

    Changed background image and removed svg

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -5,10 +5,9 @@ function Info() {
   return (
     <div className="info">
       <h1 className="info__name">Brian Sai</h1>
-      <h2 className="info__welcome">WELCOME TO MY PORTFOLIO!</h2>
       <p className="info__summary">
-        I’m a Full Stack Developer based in San Francisco Bay Area. Thanks for
-        stopping by!
+        Welcome to my portfolio! I'm a Full Stack Developer based in San
+        Francisco. Thanks for stopping by!
       </p>
       <div className="info__btn">
         <button>Learn More</button>
--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import Info from './Info';
-import { SpaceSVG } from '../img/svg';
 import './Intro.scss';
 
 function Intro() {
   return (
     <section id="intro" className="intro">
-      <div className="intro__info">
-        <Info />
-      </div>
-      <div className="intro__svg">
-        <SpaceSVG />
-      </div>
+      <Info />
     </section>
   );
 }
